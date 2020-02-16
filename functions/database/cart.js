76ed799fe397a442ed5f608bf483c0f03c8ef5e6
Shivam Singla <shivam.cse17@nituk.ac.in>
2020-02-16 22:59:13 +0530

    add cart functions

--- functions/database/cart.js
+++ functions/database/cart.js
@@ -1,7 +1,9 @@
 const {getData, addData} = require('./api')
 
+const CART_ROOT_COLLECTION = "sessions"
+
 /**
- *
+ * Resets the whole items in the cart with [cart] associated with [sessionId]
  * @param {string} sessionId
  * @param {{product_id: string, quantity: number}[]} cart
  * @return {Promise<T[]>}
@@ -9,13 +11,69 @@ const {getData, addData} = require('./api')
 exports.setCart = async function (sessionId, cart) {
   let promises = cart.map(product =>
     addData({
-        path: `sessions/${sessionId}/cart/${product.product_id}`,
+        path: `${CART_ROOT_COLLECTION}/${sessionId}/cart/${product.product_id}`,
         value: product
       },
       false
     )
   )
   return await Promise.all(promises)
+}
+
+/**
+ * Remove single product with id [productId] from the cart associated with [sessionId]
+ * @param {string} sessionId
+ * @param {string} productId
+ * @return {Promise<T>}
+ */
+exports.removeCartItem = async function(sessionId, productId) {
+  return await _removeCartItem(sessionId, productId)
+}
+
+/**
+ * Clears the cart associated with [sessionId]
+ * @param {string} sessionId
+ * @return {T[]}
+ */
+exports.removeCartItem = async function(sessionId) {
+  // retrieve all the products present in the cart
+  const products = await getData({
+    path: `${CART_ROOT_COLLECTION}/${sessionId}/cart`
+  })
+  // remove products from the cart one by one
+  let promises = products.map(productId => _removeCartItem(sessionId, productId))
+  return await Promise.all(promises)
+}
+
+/**
+ * Updates quantity of a product of the cart associated with [sessionId]
+ * @param {string} sessionId
+ * @param {string} productId
+ * @param {string} quantity
+ * @return {Promise<T>}
+ */
+exports.setCartItem = async function(sessionId, productId, quantity) {
+  return await addData({
+    path: `${CART_ROOT_COLLECTION}/${sessionId}/cart/${productId}`,
+    value: {
+      quantity
+    },
+    update: true
+  })
+}
+
+/**
+ *
+ * @param {string} sessionId
+ * @param {string} productId
+ * @return {Promise<T>}
+ * @private
+ */
+function _removeCartItem(sessionId, productId) {
+  return addData({
+    path: `${CART_ROOT_COLLECTION}/${sessionId}/cart/${productId}`,
+    delete: true
+  })
 }
 
 /**
