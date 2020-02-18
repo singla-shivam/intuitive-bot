4ab94d654b91053decf75a9ebff5526eb8f4817b
Satyam <satyamkrishikesh@gmail.com>
2020-02-18 10:11:33 +0530

    choomantar function added

--- functions/index.js
+++ functions/index.js
@@ -32,36 +32,6 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(async (request
     agent.add(`I'm sorry, can you try again?`);
   }
 
-
-  // // Uncomment and edit to make your own intent handler
-  // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
-  // // below to get this function to be run when a Dialogflow intent is matched
-  // function yourFunctionHandler(agent) {
-  //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
-  //   agent.add(new Card({
-  //       title: `Title: this is a card title`,
-  //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
-  //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
-  //       buttonText: 'This is a button',
-  //       buttonUrl: 'https://assistant.google.com/'
-  //     })
-  //   );
-  //   agent.add(new Suggestion(`Quick Reply`));
-  //   agent.add(new Suggestion(`Suggestion`));
-  //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
-  // }
-
-  // // Uncomment and edit to make your own Google Assistant intent handler
-  // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
-  // // below to get this function to be run when a Dialogflow intent is matched
-  // function googleAssistantHandler(agent) {
-  //   let conv = agent.conv(); // Get Actions on Google library conv instance
-  //   conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
-  //   agent.add(conv); // Add Actions on Google library responses to your agent's response
-  // }
-  // // See https://github.com/dialogflow/fulfillment-actions-library-nodejs
-  // // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
-
   // Run the proper function handler based on the matched Dialogflow intent name
   let intentMap = new Map();
   intentMap.set('Default Welcome Intent', welcome);
--- functions/intent_handlers/productDiscovery/choomantar.js
+++ functions/intent_handlers/productDiscovery/choomantar.js
@@ -1,8 +1,9 @@
-const {setCartItem} = require("./database/cart");
+const {setCartItem} = require("../../database/cart");
+
 
 exports.choomantar = async function (agent){
     let sessionId = agent.request_.body.sessionId
     console.log("choomantar Invoked");
-    setCartItem(sessionId, 'qZFFAVPfMChskcRwxpQZ', 3)
+    await setCartItem(sessionId, 'qZFFAVPfMChskcRwxpQZ', 3)
     agent.add("Amul milk 3 packets are now in your cart")
 }
