95e24bfaf4d97ff04b77001ace744265f3193ddb
Satyam <satyamkrishikesh@gmail.com>
2020-02-21 13:27:00 +0530

    changeQty finished.
    Signed-off-by: Satyam <satyamkrishikesh@gmail.com>

--- functions/intent_handlers/cart/changeQty.js
+++ functions/intent_handlers/cart/changeQty.js
@@ -1,33 +1,57 @@
 const {findProductsByTags, getProducts} = require("../../database/product");
 const {getCart, setCartItem, removeCartItem} = require("../../database/cart");
 
+/**
+ * This method is invoked when additional tags are requested
+ */
 exports.cartReceiveExtraTags = async function (agent) {
-
   let tags = agent.parameters.tags || [] // Tags from previous request
   let extra_tags = agent.parameters.newTags || [] // Additional tags provided
-  let qty = agent.parameters.quantity // Pass on quantity from context
-  console.log("cartRecieveExtraTags Invoked", tags, extra_tags, qty);
+  let qty = agent.parameters.quantity  // Pass on quantity from context
+  qty = qty === "" ? undefined : qty // Context sets quantity as empty string when
+  // quantity parameter is passed as undefined by intent that activated the context
+  // console.log("cartRecieveExtraTags Invoked", tags, extra_tags, qty);
   await _modifyItemQty(agent, [...tags, ...extra_tags], qty)
 }
 
+/**
+ * This method is invoked when user provides quantity missing in original request
+ */
+exports.cartConfirmQty = async function (agent) {
+  let tags = agent.parameters.tags || [] // Tags from previous request
+  let qty = agent.parameters.quantity // Quantity provided by user
+  //console.log("confirmQty Invoked", tags, qty);
+  await _modifyItemQty(agent, tags, qty)
+}
+
+/**
+ * This method is invoked when user makes a request to change quantity of an item
+ */
 exports.cartChangeQty = async function (agent) {
-  console.log("cartChangeQty Invoked");
-  let tags = agent.parameters.tag
+  //console.log("cartChangeQty Invoked");
+  let tags = agent.parameters.tags
   let qty = agent.parameters.quantity
   await _modifyItemQty(agent, tags, qty)
 }
 
+/**
+ * This method is invoked when user makes a request to remove an item from cart
+ */
 exports.cartRemoveItem = async function (agent) {
-  let tags = agent.parameters.tag
+  let tags = agent.parameters.tags
   await _modifyItemQty(agent, tags, 0)
 }
 
+/**
+ * Helper method that finds relevant product on basis of tags provided and
+ * sets it's quantity in cart (or deletes if quantity is 0)
+ */
 async function _modifyItemQty(agent, tags, quantity) {
   let cartItems = await getCart(agent)
   let products = await findProductsByTags(tags, cartItems.map((item) => item.product_id))
-  console.log("Modify Item Qty", tags, quantity, products)
+  //console.log("Modify Item Qty", tags, quantity, products)
 
-  if (await _clarifyWhichProduct(agent, products, quantity, tags) && await _clarifyQuantity(agent, quantity, products)) {
+  if (await _clarifyWhichProduct(agent, products, quantity, tags) && await _clarifyQuantity(agent, products, quantity, tags)) {
     if (quantity > 0) {
       await setCartItem(agent, products[0].product_id, quantity)
       agent.add(`Sure thing! Now there ${quantity > 1 ? 'are' : 'is'} ${quantity} ${products[0].name} in your cart.`)
@@ -37,13 +61,21 @@ async function _modifyItemQty(agent, tags, quantity) {
   }
 }
 
+/**
+ * Helper method that deletes a single product from cart
+ * @param {Product[]} products must contain only a single product
+ */
 async function _deleteItem(agent, products) {
-  console.log("_deleteItem", products[0].product_id)
+  //console.log("_deleteItem", products[0].product_id)
   await removeCartItem(agent, products[0].product_id)
   agent.add(`I have removed ${products[0].name} from your cart. Anything else?`)
 }
 
-async function _clarifyQuantity(agent, quantity, products) {
+/**
+ * Helper method that requests user for quantity when it is missing from request
+ * @return true if quantity is present in original request, otherwise false
+ */
+async function _clarifyQuantity(agent, products, quantity, tags) {
   if (quantity === undefined) {
     // User didn't provided quantity
     agent.add(`How many ${products[0].name} do you need?`)
@@ -52,6 +84,10 @@ async function _clarifyQuantity(agent, quantity, products) {
   return quantity !== undefined // True only when quantity provided
 }
 
+/**
+ * Helper method that requests user for more clarity on the product they intended in the request
+ * @return true if we have figured out a single product that user meant to say, false otherwise
+ */
 async function _clarifyWhichProduct(agent, products, quantity, tags) {
   if (products.length === 1) {
     return true // Just one product, no clarification needed
