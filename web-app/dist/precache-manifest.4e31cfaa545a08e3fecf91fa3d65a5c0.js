6141f647c8f91cccffa0f4553d40904adc18dae8
Shivam Singla <shivam.cse17@nituk.ac.in>
2020-02-21 11:55:35 +0530

    add data bug fixed

---
+++ web-app/dist/precache-manifest.4e31cfaa545a08e3fecf91fa3d65a5c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a3e14bc7a227a6f4bc7",
+    "url": "/css/app.670d8245.css"
+  },
+  {
+    "revision": "13a9e2a294a29818731977c28c8388bf",
+    "url": "/img/wallpaper.13a9e2a2.jpg"
+  },
+  {
+    "revision": "5a035cf822230217eacec62f8e48e7fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a3e14bc7a227a6f4bc7",
+    "url": "/js/app.eedb5dd1.js"
+  },
+  {
+    "revision": "d65eb76270e86cc8c220",
+    "url": "/js/chunk-vendors.bb5831a7.js"
+  },
+  {
+    "revision": "48ff823774d73eb1d1821a338b8a13fd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
